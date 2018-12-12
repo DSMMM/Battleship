23e02e0735f49fa1dfbec841a0f5273e1b4ed3b3
Marcin Krupnik <marcin_krupnik@epam.com>
2018-12-12 11:23:32 +0100

    Dodanie testu zawsze przechodzacego w kliencie i serwerze Dodatkowo pomy zaktualizowane bo byly zle nazwy (polskie zamiast angielskich). Testy zawsze przechodzą i są tylko pod jenkinsa żeby zobaczyć że tam się znajdą. Mvn clean install przeszło bez zarzutów

---
+++ battleships-client/src/test/java/com/dsmmm/battleships/client/AnyTest.java
@@ -0,0 +1,14 @@
+package com.dsmmm.battleships.client;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AnyTest {
+
+    @Test
+    public void alwaysPass() {
+        assertTrue(true);
+    }
+}
+
+
---
+++ battleships-server/src/test/java/com/dsmmm/battleships/server/AnyTest.java
@@ -0,0 +1,15 @@
+package com.dsmmm.battleships.server;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AnyTest {
+
+    @Test
+    public void alwaysPass() {
+        assertTrue(true);
+    }
+}
+
+
