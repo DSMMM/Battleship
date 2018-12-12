e7e83d9d12f3823da74c36d5ae101c260585f0aa
Dominika Druzgala <dominika_druzgala@epam.com>
2018-12-12 13:06:51 +0100

    Poprawa pom.xml, dodanie Klienta Dodano klasę umożliwiającą łaćzenie się z serverem poprzez sockety, przeniesiono maven-jar-plugin z głównego poma do modułów z konfiguracjami dla nich odpowiednimi

---
+++ battleships-client/src/main/java/com/dsmmm/battleships/client/App.java
@@ -0,0 +1,8 @@
+package com.dsmmm.battleships.client;
+
+class App {
+    public static void main(String[] args) {
+        ClientSocket clientSocket = new ClientSocket();
+        clientSocket.clientSocket();
+    }
+}
