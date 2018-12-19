67a7990603064640e6ba3be6f7b19698993cc859
Marcin Krupnik <marcin_krupnik@epam.com>
2018-12-19 16:44:28 +0100

    Przywrócono metode main do starej wersji

--- battleships-server/src/main/java/com/dsmmm/battleships/server/App.java
+++ battleships-server/src/main/java/com/dsmmm/battleships/server/App.java
@@ -1,10 +1,7 @@
 package com.dsmmm.battleships.server;
 
-import com.dsmmm.battleships.server.board.Board;
-
 class App {
     public static void main(String[] args) {
-//        new ServerInitializer().initializeServer();
-        new Board();
+        new ServerInitializer().initializeServer();
     }
 }
